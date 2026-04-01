0a01cc39e694a9d129093f83c4c87d32fd9aee52
Vasily Melenchuk <bzzzil@gmail.com>
2026-04-01 12:36:45 +0300

    chore: testcases for url, exposing data for testsuites

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -13,6 +13,35 @@ suite('Extension Test Suite', function() {
         assert.ok(editor);
     });
 
+    test('Ensure server started', async () => {
+        const outputFile : string = 'temp.html';
+        var document = await vscode.workspace.openTextDocument(__dirname + '/../../demo.adoc');
+        assert.ok(document);
+        var editor = await vscode.window.showTextDocument(document);
+        assert.ok(editor);
+        var ext = vscode.extensions.getExtension('bzzzil.vscode-asciidoc-presentation');
+        assert.ok(ext);
+
+        const context = await ext.activate();
+        assert.equal(ext.isActive, true);
+        assert.ok(context);
+
+        // Trigger some server activity by exporting to HTML
+        var result = await vscode.commands.executeCommand('asciiDocPresentation.exportHtml', [outputFile]);
+        assert.ok(result);
+        assert.ok(fs.existsSync(outputFile));
+        fs.unlinkSync(outputFile);
+
+        const containerManager = context.containerManager;
+        assert.ok(containerManager);
+        const container = containerManager.getOrCreateContainer(editor);
+        assert.ok(container);
+        const browserUrl = await container.getBrowserUrl();
+        assert.ok(browserUrl);
+        // The URL should be something like http://127.0.0.1:12345/#/
+        assert.ok(browserUrl.match(/http:\/\/127\.0\.0\.1:\d+\/#\//));
+    });
+
     test('Export to HTML', async () => {
         const outputFile : string = 'temp.html';
         var document = await vscode.workspace.openTextDocument(__dirname + '/../../demo.adoc');
